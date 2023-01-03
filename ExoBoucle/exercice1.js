9bbc0e2460cb739737e8e1cc4ebcbeb2d36d72d6
Syndicalix <sebastienspeich@ip-172-31-18-102.eu-west-3.compute.internal>
2023-01-03 14:28:56 +0000

    m'enfin

---
+++ ExoBoucle/exercice1.js
@@ -0,0 +1,29 @@
+let i = 0;
+
+while(i <= 10) 
+{
+    console.log(i);
+    i = i + 1; 
+}
+
+let j = 25;
+
+while (j >= 20)
+{
+    console.log(j);
+    j = j-1;
+}
+
+let k = 0;
+while (k <= 20)
+{
+    if(k%2===0)
+    {
+    console.log(`${k} pairs`);
+    }
+    else
+    {
+        console.log(`${k} impairs`);
+    }
+    k++;
+}
